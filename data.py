34188d8b2dbed1941bf55c54eede2ffc4b60e2f6
davidtc8 <davidtorresc8@gmail.com>
2020-12-16 22:54:38 -0600

    Questions to ask your girlfriend in a boolean manner!

--- data.py
+++ data.py
@@ -1,14 +1,15 @@
 question_data = [
-{"text": "Is black my fav color?.", "answer": "True"},
-{"text": "I like touching your hands, rather than your feet", "answer": "False"},
-{"text": "I like the way you laugh", "answer": "True"},
-{"text": "Every 9 of month we celebrate another month together", "answer": "False"},
-{"text": "I like watching comedy movies with you, because I like the way you laugh", "answer": "True"},
-{"text": "You almost fell naked in a random motel room, and you're the only one who laughed about it", "answer": "True"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You are a jealous person.", "answer": "True"},
-{"text": "You get angry when I call 'cuchicuchi' to other imaginary girls.", "answer": "True"},
-{"text": "I don't like when you call me at night", "answer": "False"},
-{"text": "I really don't like the way you talk to me sometimes", "answer": "False"},
-{"text": "I prefer ur ass over ur boobs", "answer": "False"}
+    {"text": "Is black my fav color?.", "answer": "True"},
+    {"text": "I like touching your hands, rather than your feet", "answer": "False"},
+    {"text": "I like the way you laugh", "answer": "True"},
+    {"text": "Every 9 of month we celebrate another month together", "answer": "False"},
+    {"text": "I like watching comedy movies with you, because I like the way you laugh", "answer": "True"},
+    {"text": "You almost fell naked in a random motel room, and you're the only "
+             "one who laughed about it", "answer": "True"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You are a jealous person.", "answer": "True"},
+    {"text": "You get angry when I call 'cuchicuchi' to other imaginary girls.", "answer": "True"},
+    {"text": "I don't like when you call me at night", "answer": "False"},
+    {"text": "I really don't like the way you talk to me sometimes", "answer": "False"},
+    {"text": "I prefer ur ass over ur boobs", "answer": "False"}
 ]
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from question_model import Question
 from data import question_data
+import random
+
+question_bank = []
+
+for dic in question_data:
+    for key in dic.items():
+        question = Question(dic["text"], dic["answer"])
+    print('{}, {}'.format(question.text, question.answer))
+        #q_text = question.text + " " + question.answer
+        #print(q_text)
+        #question_bank.append(q_text)
+        #print(question_bank)
 
