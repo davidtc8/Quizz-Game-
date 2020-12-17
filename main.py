7ec0c86b1644f2d44ac376e0a909c6bdd3cf7b06
davidtc8 <davidtorresc8@gmail.com>
2020-12-17 10:50:43 -0600

    Files needed for main.py!

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from question_model import Question
 from data import question_data
-import random
+from quiz_brain import QuizBrain
 
 question_bank = []
 
-for dic in question_data:
-    for key in dic.items():
-        question = Question(dic["text"], dic["answer"])
-    print('{}, {}'.format(question.text, question.answer))
-        #q_text = question.text + " " + question.answer
-        #print(q_text)
-        #question_bank.append(q_text)
-        #print(question_bank)
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(q_text = question_text, q_answer = question_answer)
+    question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You are finished babe")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
